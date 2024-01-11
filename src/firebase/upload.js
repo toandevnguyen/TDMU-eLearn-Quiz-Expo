f7646aae2bb1d619229dc359ffcb894bc6764aac
Your Name.com <toandevnguyen@gmail.com>
2024-01-12 00:58:04 +0700

    done upload data.json to fireStore....

---
+++ src/firebase/upload.js
@@ -0,0 +1,15 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+const data = require("./data.json");
+
+async function uploadData() {
+  for (const doc of data) {
+    await db.collection("questions").add(doc);
+  }
+}
+uploadData();
