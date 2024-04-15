c69135af5801faee61c731192e0d551a1e1f5367
Your Name.com <toandevnguyen@gmail.com>
2024-04-15 18:27:08 +0700

    Revert lại trước thằng 70d4f69: refactor: :memo: tính năng Login with GG tức là thằng 607a269

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,97 +1,50 @@
 import { FontAwesome5 } from "@expo/vector-icons";
-import auth from "@react-native-firebase/auth";
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-} from "@react-native-google-signin/google-signin";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Image, StatusBar, StyleSheet, Text, View } from "react-native";
 
 import {
-  // ButtonComponent,
+  ButtonComponent,
   Container,
   // Section,
   TextComponent,
 } from "../../components";
 import { colors } from "../../constants/colors";
 import { fontFamilies } from "../../constants/fontFamilies";
-// import { globalStyles } from "../../styles/globalStyles";
+import { globalStyles } from "../../styles/globalStyles";
 
 export default function LoginScreen() {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
-
-  GoogleSignin.configure({
-    webClientId:
-      "727064933607-sibinqt89eh0g7glqrau2jo42jc8dmpe.apps.googleusercontent.com",
-  });
-  // Handle user state changes
-  function onAuthStateChanged() {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
-
-  const onGoogleButtonPress = async () => {
-    // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn();
-
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-    // Sign-in the user with the credential
-    const user_signIn = auth().signInWithCredential(googleCredential);
-    user_signIn
-      .then((user) => {
-        console.log(user);
-        // return auth().signInWithCredential(googleCredential);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  if (initializing) return null;
-
-  if (!user) {
-    return (
-      <Container>
-        <Image
-          source={require("../../assets/images/LoginScreenImg.png")}
-          resizeMode="cover"
-          style={{ width: "100%", height: 220 }}
+  const [email, setEmail] = useState("");
+  return (
+    <Container>
+      <Image
+        source={require("../../assets/images/LoginScreenImg.png")}
+        resizeMode="contain"
+        style={{ width: "100%", height: 220 }}
+      />
+      <View style={styles.container}>
+        <TextComponent
+          text={"Welcome to\nTDMU-eLearn-Quiz "}
+          type="title"
+          color="black"
+          font={fontFamilies.bold}
+          size={30}
+          styles={{
+            textAlign: "center",
+            marginVertical: 80,
+          }}
         />
-        <View style={styles.container}>
-          <TextComponent
-            text={"Welcome to\nTDMU-eLearn-Quiz "}
-            type="title"
-            color="black"
-            font={fontFamilies.bold}
-            size={30}
-            styles={{
-              textAlign: "center",
-              marginVertical: 80,
-            }}
-          />
-          <TextComponent
-            text="Đăng nhập/Đăng ký!"
-            type="description"
-            color="black"
-            font={fontFamilies.bold}
-            size={20}
-            styles={{
-              textAlign: "center",
-              marginVertical: 20,
-            }}
-          />
-          <GoogleSigninButton onPress={onGoogleButtonPress} />
-          {/* <ButtonComponent
+        <TextComponent
+          text="Đăng nhập/Đăng ký!"
+          type="description"
+          color="black"
+          font={fontFamilies.bold}
+          size={20}
+          styles={{
+            textAlign: "center",
+            marginVertical: 20,
+          }}
+        />
+        <ButtonComponent
           icon={<FontAwesome5 name="google" color={colors.white} size={24} />}
           onPress={() => {
             console.log("press");
@@ -101,17 +54,10 @@ export default function LoginScreen() {
             marginBottom: 230,
             paddingHorizontal: 20,
           }}
-          text="   Đăng nhập bằng Google"
-        /> */}
-        </View>
-      </Container>
-    );
-  }
-
-  return (
-    <View>
-      <Text>Welcome {user}</Text>
-    </View>
+          text="   Đăng nhập bằng Google "
+        />
+      </View>
+    </Container>
   );
 }
 
